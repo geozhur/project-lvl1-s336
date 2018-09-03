cc825f2687db33badd118b34bc709c6369380e41
Santa Klaus <egorich2004@mail.ru>
2018-09-03 21:19:18 +0200

    add class

--- src/Cli.php
+++ src/Cli.php
@@ -1,12 +1,16 @@
 <?php
+
 namespace BrainGames\Cli;
 
 use function \cli\line;
 
-function run() 
-{
-    line('Welcome to the Brain Game!');
-    line();
-    $name = \cli\prompt('May I have your name?');
-    line("Hello, %s!", $name);
+class Cli 
+{ 
+    public function run() 
+    {
+        line('Welcome to the Brain Game!');
+        line();
+        $name = \cli\prompt('May I have your name?');
+        line("Hello, %s!", $name);
+    }
 }
