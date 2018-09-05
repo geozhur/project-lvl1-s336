99d4f29bd14a0b2717b3cfd38378d1b5650c241e
Santa Klaus <egorich2004@mail.ru>
2018-09-05 13:15:40 +0200

    fix gcd

--- src/games/Gcd.php
+++ src/games/Gcd.php
@@ -4,23 +4,27 @@ namespace BrainGames\Gcd;
 
 use function \BrainGames\Common\runTextGame;
 
+function getGcd($num1, $num2)
+{
+    while ($num1 != $num2) {
+        if ($num1 > $num2) {
+            $num1 = $num1 - $num2;
+        } else {
+            $num2 = $num2 - $num1;
+        }
+    }
+
+    return $num2;
+}
+
 function getQuestionAndRightAnswerGcd($textQuestion)
 {
     $num1 = rand(1, 100);
     $num2 = rand(1, 100);
-    $n1 = $num1;
-    $n2 = $num2;
 
-    while ($n1 != $n2) {
-        if ($n1 > $n2) {
-            $n1 = $n1 - $n2;
-        } else {
-            $n2 = $n2 - $n1;
-        }
-    }
-
+    $rightAnswer = getGcd($num1, $num2);
     $textQuestion .= $num1 . " " . $num2;
-    return [$textQuestion, $n2];
+    return [$textQuestion, $rightAnswer];
 }
 
 function runGcd()
